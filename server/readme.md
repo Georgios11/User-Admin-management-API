c8ec0dc0ef3b0dd2d862af7a47fe0fe41e3a4774
Georgios11 <tsompanidis.georgios@gmail.com>
2023-04-14 15:58:43 +0200

    modules removed

--- server/readme.md
+++ server/readme.md
@@ -2,3 +2,11 @@
 
 - Initial folder structure
 - Initial environmental variable
+
+## NPMs
+
+- Express
+- dotenv
+- morgan
+- nodemon
+- mongoose
--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const dev = require("./config");
 
 const app = express();
 
 const PORT = dev.app.serverPort;
 
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "API is running",
+  });
+});
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
